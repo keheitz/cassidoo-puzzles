1c0a3c095cb7069fa7daf2ee49232f6d5736728f
Kelly Heitz <keheitz@gmail.com>
2023-08-16 21:11:28 -0500

    add luhn algorithm implementation for 312

--- 312_luhn_algorithm/credit_card.py
+++ 312_luhn_algorithm/credit_card.py
@@ -2,14 +2,38 @@
 
 class CreditCard:
     """Class representing a credit card"""
+    credit_card_types = {
+                            '3': 'american express',
+                            '4': 'visa',
+                            '5': 'mastercard',
+                            '6': 'discover'
+                        }
 
     def __init__(self, card_number):
         self.card_number = card_number
+        self.check_digit = card_number % 10 if card_number > 9 else 0
+        self.payload = card_number // 10 if card_number > 9 else card_number
 
-    def luhn_check(self):
+    def luhn_check_is_valid(self):
         """Check if card number is valid under luhn algorithm"""
-        return False
+        payload_array = list(map(int, str(self.payload)))
+        payload_array.reverse() # start with last digit
+        # Multiply even indexed numbers by 2, odd by 1
+        multiplied_vals = [num * 2 for i, num in enumerate(payload_array) if not i % 2] + [
+            num for i, num in enumerate(payload_array) if i % 2
+        ]
+        multiplier_sum = 0
+        # Add the multiplied value to sum if it's single digit (less than 10),
+        # otherwise get and add each digit individually
+        for val in multiplied_vals:
+            if val < 10:
+                multiplier_sum += val
+            else:
+                multiplier_sum += (val % 10)
+                multiplier_sum += (val // 10)
+        return (10 - multiplier_sum % 10) == self.check_digit
 
     def card_type(self):
         """Returns the card type - visa, mastercard, or discover"""
-        return 'visa'
+        first_digit = str(self.card_number)[0]
+        return self.credit_card_types[first_digit]
--- 312_luhn_algorithm/test_credit_card.py
+++ 312_luhn_algorithm/test_credit_card.py
@@ -3,12 +3,24 @@ from credit_card import CreditCard
 
 def test_luhn_check_false():
     """Test luhn_check on invalid credit card number"""
-    assert not CreditCard(123456789).luhn_check()
+    assert not CreditCard(123456789).luhn_check_is_valid()
 
 def test_luhn_check_true():
     """Test luhn_check on valid credit card number"""
-    assert CreditCard(5555555555554444).luhn_check()
+    assert CreditCard(5555555555554444).luhn_check_is_valid()
 
-def test_card_type():
+def test_luhn_check_true_no_auth():
+    """Test luhn_check on valid credit card number"""
+    assert CreditCard(4000002760003184).luhn_check_is_valid()
+
+def test_card_type_mastercard():
     """Check that credit card type method returns correct brand"""
     assert CreditCard(5555555555554444).card_type() == "mastercard"
+
+def test_card_type_visa():
+    """Check that credit card type method returns correct brand"""
+    assert CreditCard(4000002760003184).card_type() == "visa"
+
+def test_card_type_discover():
+    """Check that credit card type method returns correct brand"""
+    assert CreditCard(6011000998034767).card_type() == "discover"
